4170f6f699f1790bfb43cb652c173cc16bbbe8be
UnicornBear <jtbregenzer@gmail.com>
2019-04-25 20:52:40 -0600

    loop funtion with outputs to html

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,6 +4,12 @@ var pf = new petfinder.Client({
   secret: "W9M0tIJ52td2d3TzuuhOAiGPT9mKWmGzHMIbKa0Q"
 });
 
+
+
+
+
+
+
 $("#button-submit").on("click", function() {
   // on submit button search for the pets by zip
   event.preventDefault();
@@ -16,38 +22,52 @@ $("#button-submit").on("click", function() {
   // else (plant)
 
 
-  pf.animal.search({ location: userZip })
+  pf.animal.search({ 
+    location: userZip,
+  
+  })
     // can i insert here the val from searchig var at the screen?
     
     .then(response => {
+      // $("state").empty();
       for (var i=0; i<5; i++){
 
       // Do something with `response.data.animals`
       console.log(response);
 
       var animalAge = response.data.animals[i].age;
-      console.log("this is the dog in array" + animalAge);
+      // console.log("this is the dog in array" + animalAge);
 
-      var animalPhoto = response.data.animals[i].photos[i].medium;
-      console.log(animalPhoto);
+      // var animalPhoto = response.data.animals[i].photos[i].small;
+      // console.log(animalPhoto);
 
       var animalName = response.data.animals[i].name;
-      console.log(animalName);
+      // console.log(animalName);
 
       var animalState = response.data.animals[i].contact.address.state;
       console.log(animalState);
 
-      var animalDiscription = response.data.animals[i].description;
-      console.log(animalDiscription);
+      // var animalDiscription = response.data.animals[i].description;
+      // console.log(animalDiscription);
+      
+      var responseAnimal = $("<p>");
+          responseAnimal.text(animalState);
+          responseAnimal.text(animalName);
+          responseAnimal.text(animalAge);
+
+        $("#responseAnimal").append("Animal Named: " + animalName + "is available at " + animalState + animalAge);
+
       }
+
 
     })
     .catch(function(error) {
       console.log(error);
       // Handle the error
-    
-    })
+    });
   
-    $("#animalLOC").append(animalState);
+    
 
   });
+
+ 
