f25fef1327cc915356714f7ca6612dfa1a33155e
UnicornBear <46576621+UnicornBear@users.noreply.github.com>
2019-04-27 11:57:20 -0600

    JS updates
    species changed to type for search
    cleaned up the response photo and animal div var groupings

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -17,18 +17,11 @@ $(function() {
   $("#Match").on("click", function() {
     event.preventDefault();
 
-    var userZip = $("#zip")
-      .val()
-      .trim();
-    var userAge = $("#age")
-      .val()
-      .trim();
-    var userSpecies = $("#species")
-      .val()
-      .trim();
-    var userGender = $("#gender")
-      .val()
-      .trim();
+    var userZip = $("#zip").val().trim();
+    var userAge = $("#age").val().trim();
+    var userSpecies = $("#species").val().trim();
+    var userGender = $("#gender").val().trim();
+    
     console.log("this is user Zip" + userZip);
     console.log("this is Age " + userAge);
     console.log("this is Species " + userSpecies);
@@ -42,7 +35,7 @@ $(function() {
       .search({
         location: userZip,
         age: userAge,
-        species: userSpecies,
+        type: userSpecies,
         gender: userGender
       })
       .then(response => {
@@ -54,39 +47,39 @@ $(function() {
           //if to check for picture, no picture, not included
           if (!response.data.animals[i].photos[0]) {
             continue }
+          
           var animalPhoto = response.data.animals[i].photos[0].small;
 
           var animalAge = response.data.animals[i].age;
           var animalName = response.data.animals[i].name;
+          var animalCity = response.data.animals[i].contact.address.city;
           var animalState = response.data.animals[i].contact.address.state;
           var animalAddress = response.data.animals[i].contact.address.address1;
           var animalDiscription = response.data.animals[i].description;
 
-          var animalDiv = $("<div class='col-md-3'>");
-
-          animalDiv.addClass("for-pets");
-          
           var responsePhoto = $("<img>");
-          responsePhoto.attr("src", animalPhoto);
-          responsePhoto.addClass("animal-pic");
-          animalDiv.append(responsePhoto);
+              responsePhoto.attr("src", animalPhoto);
+              responsePhoto.addClass("animal-pic");
 
-          var responseName = $("<p>");
-          responseName.text(animalName);
-          var responseAge = $("<p>");
-          responseAge.text(animalAge);
-          var responseState = $("<p>");
-          responseState.text(animalState);
-          var responseAddress = $("<p>");
-          responseAddress.text(animalAddress);
-          var responseDiscription = $("<p>");
-          responseDiscription.text(animalDiscription);
-          
+          var animalDiv = $("<div class='col-md-3'>");
+              animalDiv.append(responsePhoto);
+              animalDiv.addClass("for-pets");
+              animalDiv.append(animalName);
+              animalDiv.append(animalCity);
+              animalDiv.append(animalState);
+              animalDiv.append(animalAddress);
+              animalDiv.append(animalDiscription);        
 
-          animalDiv.append(animalName);
-          animalDiv.append(animalState);
-          animalDiv.append(animalAddress);
-          animalDiv.append(animalDiscription);
+          // var responseName = $("<p>");
+          // responseName.text(animalName);
+          // var responseAge = $("<p>");
+          // responseAge.text(animalAge);
+          // var responseState = $("<p>");
+          // responseState.text(animalState);
+          // var responseAddress = $("<p>");
+          // responseAddress.text(animalAddress);
+          // var responseDiscription = $("<p>");
+          // responseDiscription.text(animalDiscription);
 
           $("#responseAnimal").prepend(animalDiv);
          
